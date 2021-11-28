797f4bfb7ea81e839f0a840cd0548bffe2586957
wangyue <240498767@qq.com>
2021-11-28 11:27:11 +0800

    Update run.py

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ if "TG_USER_ID"in os .environ and os .environ ["TG_USER_ID"]:#line:5
         print ("Telegram 推送打开")#line:8
 def fristlogin ():#line:10
     O000OO0OO000000OO ='bdEH5jnTfBaGhFywTMIscbf{a}w{b}i{c}{d}{e}P/w2SIZlbirzi7yJ2m'.format (a =random .randint (1 ,9 ),b =random .randint (1 ,9 ),c =random .choice ('ABCDEFG'),d =random .choice ('ABCDEFGH'),e =random .choice ('ABCDEFG'))#line:11
-    O00OOOO0OOOOO0000 ={'Accept':'application/json, text/plain, */*','Accept-Encoding':'gzip, deflate, br','Accept-Language':'zh-cn','Connection':'keep-alive','Host':'pwb.wubabanjia.net','Origin':'https://web.wubabanjia.net','User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 Mobile/15E176 Safari/604.1','X-AFDAC809-9AD768A3':O000OO0OO000000OO }#line:20
-    O00OOO0000OO0O000 ='https://pwb.wubabanjia.net/v1/fastlogin'#line:21
+    O00OOOO0OOOOO0000 ={'Accept':'application/json, text/plain, */*','Accept-Encoding':'gzip, deflate, br','Accept-Language':'zh-cn','Connection':'keep-alive','Host':'pwc-ws.antaizhongcheng.com','Origin':'https://web.wubabanjia.net','User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 Mobile/15E176 Safari/604.1','X-AFDAC809-9AD768A3':O000OO0OO000000OO }#line:20
+    O00OOO0000OO0O000 =https://web-aws.antaizhongcheng.com/v1/fastlogin'#line:21
     O0O0O0O00OOOOO00O =0 #line:22
     while O0O0O0O00OOOOO00O <=10 :#line:23
         O0O0O0O00OOOOO00O +=1 #line:24
@@ -31,7 +31,7 @@ def register_email (OOO000OOOOO00O000 ):#line:38
     O0OO00OO00OOO000O ='{}@qq.com'.format (random .randint (1000000 ,99999999 ))#line:39
     print (O0OO00OO00OOO000O )#line:40
     OOO00O00OOO00OO00 ={'platform':(None ,'uj72624275'),'email':(None ,O0OO00OO00OOO000O ),'password':(None ,'sdEWWEqqCEWW@!')}#line:45
-    O0OO00O0O0O000OO0 ='https://pwb.wubabanjia.net/v1/register/email?token={}'.format (OOO000OOOOO00O000 )#line:46
+    O0OO00O0O0O000OO0 ='https://web-aws.antaizhongcheng.com/v1/register/email?token={}'.format (OOO000OOOOO00O000 )#line:46
     O0O0OO00O00OOO00O =requests .post (O0OO00O0O0O000OO0 ,files =OOO00O00OOO00OO00 )#line:48
 def main ():#line:84
     if 'START' in os.environ and os.environ["START"] =='JKASNDWKND':
@@ -59,14 +59,15 @@ def telegram_bot (O0O0O0OOOOOOO0O00 ,O0O00O000000O0O00 ):#line:50
 def invite (OO0OO0OOOO00000OO ):#line:66
     OOO0OOOO00OO0OO00 =os .environ ["INVITECODE"]#line:67
     OO0O0OOO0000OO00O ={'code':(None ,OOO0OOOO00OO0OO00 )}#line:68
-    O00OOOO000O00OO00 ='https://pwb.wubabanjia.net/v1/user/invite?token={}'.format (OO0OO0OOOO00000OO )#line:69
+    O00OOOO000O00OO00 ='https://pwc-ws.antaizhongcheng
+/v1/user/invite?token={}'.format (OO0OO0OOOO00000OO )#line:69
     OO00OOOOO000O0O00 =requests .post (O00OOOO000O00OO00 ,files =OO0O0OOO0000OO00O )#line:71
     print (OO00OOOOO000O0O00 .text )#line:72
     choice_vip ()#line:73
 def choice_vip ():#line:74
     if 'TOKEN'in os .environ :#line:76
         O000OO0OO000O0OO0 =os .environ ["TOKEN"]#line:77
-        O0O0000OO000OO0OO ='https://pwb.sjzrongshida.cn/v1/lottery/1?token={}'.format (O000OO0OO000O0OO0 )#line:78
+        O0O0000OO000OO0OO ='https://pwc-ws.antaizhongcheng.com/v1/lottery/1?token={}'.format (O000OO0OO000O0OO0 )#line:78
         OO0O00OO000O00O00 =requests .post (O0O0000OO000OO0OO )#line:79
         time .sleep (2 )#line:80
         telegram_bot ('lutube','邀请完成！随机抽奖完成！')#line:81
